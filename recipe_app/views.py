c6c4c7d707094f1c8e8ce780ad2803f9498fc7a1
JalalBelsifar <jalal.belsifar@gmail.com>
2021-09-11 20:51:47 -0500

    decorators

--- recipe_app/views.py
+++ recipe_app/views.py
@@ -10,18 +10,18 @@ from django.contrib.admin.views.decorators import staff_member_required
 def index(request):
     recipes = Recipes.objects.all()
     return render(request, 'index.html', {'recipes': recipes})
-@login_required
+# @login_required
 def author_detail(request, author_id: int):
     my_author = Author.objects.get(id=author_id)
     author_post = Recipes.objects.filter(author=my_author)
     return render(request, 'author_detail.html', {'author': my_author, 'recipes': author_post})
-@login_required
+# @login_required
 def recipes_detail(request, recipe_id: int):
     recipe = Recipes.objects.get(id=recipe_id)
     return render(request, 'recipes_detail.html', {'recipes': recipe})
 
 
-@login_required
+# @login_required
 def add_recipes(request):
     if request.method == 'POST':
         form = AddRecipesForm(request.POST)
